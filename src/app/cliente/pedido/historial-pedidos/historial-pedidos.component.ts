48affe3518341d50796adc3e2c09d1234d8024ee
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-11-22 18:05:39 -0500

    feat: add function comprar nuevamente (historial compra)

--- src/app/cliente/pedido/historial-pedidos/historial-pedidos.component.ts
+++ src/app/cliente/pedido/historial-pedidos/historial-pedidos.component.ts
@@ -6,7 +6,10 @@ import { CommonModule } from '@angular/common';
 import { SolesPipe } from '../../../soles.pipe';
 import { NavbarComponent } from "../../layout/navbar/navbar.component";
 import { FooterComponent } from "../../layout/footer/footer.component";
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CarritoService } from '../../carrito/services/carrito.service';
+import { forkJoin } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-historial-pedidos',
@@ -27,7 +30,9 @@ export class HistorialPedidosComponent implements OnInit {
 
   constructor(
     private pedidoService: PedidoService,
-    private authService: AuthService
+    private authService: AuthService,
+    private carritoService: CarritoService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -64,6 +69,38 @@ export class HistorialPedidosComponent implements OnInit {
       this.lowIndex -= this.pageSize;
       this.highIndex -= this.pageSize;
     }
+  }
+
+  comprarNuevamente(pedido: PedidoResponse) {
+    this.loading = true;
+
+    const peticiones = pedido.detalle.map((d) => 
+      this.carritoService.agregarProducto({
+        productoId: d.productoId,
+        cantidad: d.cantidad
+      })
+    );
+
+    // Ejecutar todas las peticiones y esperar a que terminen
+    forkJoin(peticiones).subscribe({
+      next: () => {
+        this.loading = false;
+
+        Swal.fire({
+          title: '¡Listo!',
+          text: 'Los productos han sido añadidos a tu carrito.',
+          icon: 'success',
+          confirmButtonText: 'Ir al carrito'
+        }).then(() => {
+          this.router.navigate(['/carrito'])
+        })
+      },
+      error: (err) => {
+        this.loading = false;
+        console.error('Error al agregar productos al carrito', err);
+        Swal.fire('Error', 'No se pudieron agregar los productos al carrito.', 'error');
+      }
+    })
   }
 
 }
