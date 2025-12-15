df52555e486d73bef7d99dcc0d0b47a5f4df132e
Daniel-JA03 <danieljaimesamancio@gmail.com>
2025-12-14 19:04:27 -0500

    feat: Add pagination to product

--- src/app/cliente/producto/producto.component.ts
+++ src/app/cliente/producto/producto.component.ts
@@ -28,7 +28,15 @@ export class ProductoComponent implements OnInit {
   categorias: CategoriaFiltro[] = []
   categoriasSeleccionadas: string[] = []
 
-  productoBase: ProductoResponse[] = [];
+  productosBase: ProductoResponse[] = [];
+
+  // paginacion
+  pageSize = 6; // 3x3 grid
+  lowIndex = 0;
+  highIndex = this.pageSize;
+
+  productosFiltrados: ProductoResponse[] = [];
+
 
   constructor(
     private productoService: ProductoService,
@@ -44,24 +52,23 @@ export class ProductoComponent implements OnInit {
   cargarProductos() {
     this.productoService.obtenerListaProductos().subscribe({
       next: (data: ProductoResponse[]) => {
-      
+
         // solo productos con stock
-        this.productoBase = data.filter(p => p.stock > 0);
-        
-        // grid inicial
-        this.productosDestacados = this.productoBase.map(p => ({
-          ...p,
-          imagenUrl: 'assets/images/default.jpg'
-        }));
+        this.productosBase = data.filter(p => p.stock > 0);
+
+        this.productosFiltrados = this.productosBase;
 
         // categorias siempre desde el total disponible
-        this.actualizarCategorias(this.productoBase);
+        this.actualizarCategorias(this.productosBase);
 
-  
+        this.resetPagination();
+        this.actualizarGrid();
+
+
         // Volver a asignar imagenes si ya se cargaron
-        if (this.imagenes.length > 0) {
-          this.asignarImagenes()
-        }
+        // if (this.imagenes.length > 0) {
+        //   this.asignarImagenes()
+        // }
       },
       error: (err) => {
         console.error('Error al cargar productos:', err);
@@ -101,7 +108,7 @@ export class ProductoComponent implements OnInit {
 
     this.productosDestacados = this.productosDestacados.map(producto => {
       const imagen = this.imagenes.find(img => img.productoId === producto.idProducto);
-      
+
       console.log(`Buscando imagen para productoId ${producto.idProducto}:`, imagen); // 🔍 Depuración
 
       return {
@@ -150,7 +157,7 @@ export class ProductoComponent implements OnInit {
   //  filtrar por categorias los productos
   onCategoriaChange(event: Event, categoria: string) {
     const checked = (event.target as HTMLInputElement).checked;
-  
+
     if (checked) {
       this.categoriasSeleccionadas.push(categoria);
     } else {
@@ -162,17 +169,17 @@ export class ProductoComponent implements OnInit {
 
   filtrarPorCategorias() {
     // Si no hay filtros -> traer todos los productos
-    if (this.categoriasSeleccionadas.length === 0) {
-      this.productosDestacados = this.productoBase.map(p => ({
+    /*if (this.categoriasSeleccionadas.length === 0) {
+      this.productosDestacados = this.productosBase.map(p => ({
         ...p,
         imagenUrl: 'assets/images/default.jpg'
     }));
 
     this.asignarImagenes();
       return;
-    }
+    }*/
 
-    const filtrados = this.productoBase.filter(p =>
+    /*const filtrados = this.productoBase.filter(p =>
       this.categoriasSeleccionadas.includes(p.nombreCategoria)
     );
 
@@ -181,7 +188,17 @@ export class ProductoComponent implements OnInit {
       imagenUrl: 'assets/images/default.jpg'
     }));
 
-    this.asignarImagenes();
+    this.asignarImagenes();*/
+
+    this.productosFiltrados =
+    this.categoriasSeleccionadas.length === 0
+      ? this.productosBase
+      : this.productosBase.filter(p =>
+          this.categoriasSeleccionadas.includes(p.nombreCategoria)
+        );
+
+    this.resetPagination();
+    this.actualizarGrid();
   }
 
   actualizarCategorias(productos: ProductoResponse[]) {
@@ -199,5 +216,41 @@ export class ProductoComponent implements OnInit {
       ([nombre, total]) => ({ nombre, total })
     );
   }
+
+  // paginacion
+  resetPagination() {
+    this.lowIndex = 0;
+    this.highIndex = this.pageSize;
+  }
+
+  actualizarGrid() {
+    this.productosDestacados = this.productosFiltrados
+      .slice(this.lowIndex, this.highIndex)
+      .map(p => ({
+        ...p,
+        imagenUrl: 'assets/images/default.jpg'
+      }));
+
+    if (this.imagenes.length > 0) {
+      this.asignarImagenes();
+    }
+  }
+
+  nextPage() {
+    if (this.highIndex < this.productosFiltrados.length) {
+      this.lowIndex += this.pageSize;
+      this.highIndex += this.pageSize;
+      this.actualizarGrid();
+    }
+  }
+
+  prevPage() {
+    if (this.lowIndex > 0) {
+      this.lowIndex -= this.pageSize;
+      this.highIndex -= this.pageSize;
+      this.actualizarGrid();
+    }
+  }
+
 
 }
